e26402b4ea312b866549886a9b640b031d4ebb92
NurkaAmre <nurkerey30@gmail.com>
2024-10-05 19:33:35 +0600

    Setup express server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/hello', (req, res) => {
+  res.send('Task Manager app!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
