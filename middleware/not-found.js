458b768753fb72c7a7bf8764767b0306c8e5b67d
NurkaAmre <nurkerey30@gmail.com>
2024-11-06 00:22:59 +0600

    Async wrapper

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,7 +1,5 @@
-const notFound = (req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
+const notFound = (req, res) => {
+  res.status(404).send('Route does not exist');
 };
 
 module.exports = notFound;
